19a6e191c96932161a362e3886c7aecae86b0745
Samuel Beard <samjb2@gmail.com>
2021-08-05 10:24:40 +0100

    release

--- dist/HeaderRow/index.js
+++ dist/HeaderRow/index.js
@@ -3,13 +3,27 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "HeaderRow", {
-  enumerable: true,
-  get: function get() {
-    return _HeaderRow.default;
-  }
-});
+exports.default = void 0;
 
-var _HeaderRow = _interopRequireDefault(require("./HeaderRow"));
+var _react = _interopRequireDefault(require("react"));
+
+var _reactBootstrap = require("react-bootstrap");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var HeaderRow = function HeaderRow(_ref) {
+  var title = _ref.title,
+      page = _ref.page;
+  return page ? /*#__PURE__*/_react.default.createElement(_reactBootstrap.Row, {
+    className: "mt-5"
+  }, /*#__PURE__*/_react.default.createElement(_reactBootstrap.Col, null, /*#__PURE__*/_react.default.createElement("h1", null, title))) : /*#__PURE__*/_react.default.createElement(_reactBootstrap.Row, {
+    className: "mt-7"
+  }, /*#__PURE__*/_react.default.createElement(_reactBootstrap.Col, null, /*#__PURE__*/_react.default.createElement("h2", {
+    className: "mb-0"
+  }, title), /*#__PURE__*/_react.default.createElement("hr", {
+    className: "mt-0"
+  })));
+};
+
+var _default = HeaderRow;
+exports.default = _default;
--- src/components/HeaderRow/index.js
+++ src/components/HeaderRow/index.js
@@ -1,3 +1,21 @@
-import HeaderRow from './HeaderRow'
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
 
-export { HeaderRow }
+const HeaderRow = ({ title, page }) => {
+    return page ? (
+        <Row className='mt-5'>
+            <Col>
+                <h1>{title}</h1>
+            </Col>
+        </Row>
+    ) : (
+        <Row className='mt-7'>
+            <Col>
+                <h2 className='mb-0'>{title}</h2>
+                <hr className='mt-0' />
+            </Col>
+        </Row>
+    )
+}
+
+export default HeaderRow
